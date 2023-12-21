bf7a3fc45e1578fb57aa53431ca23b81c0d98cd5
Steven Mentzer <stevenfmentzer@gmail.com>
2023-12-21 11:52:12 -0500

    added controlled form

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -24,9 +24,9 @@ function NewPlantForm( { addNewPlant } ) {
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} type="text" name="name" placeholder="Plant name" />
-        <input onChange={handleChange} type="text" name="image" placeholder="Image URL" />
-        <input onChange={handleChange} type="number" name="price" step="0.01" placeholder="Price" />
+        <input onChange={handleChange} type="text" name="name" placeholder="Plant name" value={formData.name} />
+        <input onChange={handleChange} type="text" name="image" placeholder="Image URL" value={formData.image} />
+        <input onChange={handleChange} type="number" name="price" step="0.01" placeholder="Price" value={formData.price}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
